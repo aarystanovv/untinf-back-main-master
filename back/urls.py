e1e64acfed32e4e9cd303576f529db5414c7141b
Nurkhat Muratkan <nurhatmurathan001@gmail.com>
2023-09-23 21:10:26 +0600

    vercel conf ci/cd

--- back/urls.py
+++ back/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 from back import settings
 
@@ -24,5 +25,4 @@ urlpatterns = [
     path('api/', include('api.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
